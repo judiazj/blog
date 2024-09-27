282be525b3856bb0b777ecb457281d98547105b7
Judiazj <judiazj@unal.edu.co>
2024-09-27 01:34:54 -0500

    obtener blogs

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -14,15 +14,8 @@ export class BlogService {
         return newPost;
     }
 
-    getPosts() {
-        return [{
-            "id": 1,
-            "title": "My First Blog Post",
-            "content": "This is the content of my first blog post.",
-            "category": "Technology",
-            "tags": ["Tech", "Programming"],
-            "createdAt": "2021-09-01T12:00:00Z",
-            "updatedAt": "2021-09-01T12:00:00Z"
-        }]
+    async getPosts(): Promise<Blog[]> {
+        const blogs = await this.blogModel.find();
+        return blogs
     }
 }
